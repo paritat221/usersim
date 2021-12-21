42e1059620a4772fc4e612edbf6015756887e1ab
paritat221 <95929068+paritat221@users.noreply.github.com>
2021-12-21 14:15:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 const {token} = require('./config.json');
-const prefix = "g. ";
 
+const prefix = "g. ";
+const channels = ['680537216761724945','748563164362440856','720686393415827506','906485897619460173'];
 client.on('ready', async () => {
     console.log(':troll:');
 });
@@ -10,18 +11,16 @@ client.on('ready', async () => {
 client.on('message', async (message) => {
     const channel = message.channel;
     var u_messages = [];
-    if(channel.id=='748563164362440856'){ //restricts channels able to use it, change it to if(true) for global access
         if(message.content.startsWith(prefix)){
-            var um = await fetch_msg(message, client);
+            var um = await fetch_msg(message);
             var um_content = um.map(el => el.content);
             let mention = message.mentions.users.first();
             if (!mention) mention = message.author;
             message.channel.send(`${mention} : ${gen_msg(um_content)}`);
         }
-    }
 });
 
-const fetch_msg = async (message, client) => {
+const fetch_msg = async (message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     let mention = message.mentions.users.first();
     if (!mention) mention = message.author;
@@ -31,7 +30,7 @@ const fetch_msg = async (message, client) => {
     await message.channel.fetchMessages({limit:100}).then(async msgs=>{
         lm_id = msgs.first().id;
     });
-    while(n_um.length<100){ //number of messages to fetch, changing it to more can cause the bot to become slower
+    while(n_um.length<100){
         await message.channel.fetchMessages({limit:100, before:lm_id}).then(async msgs=>{
             msgs.forEach(async msg =>{
                 um.push(msg);
@@ -57,4 +56,4 @@ gen_msg = (messages)=>{
     return sentence.join(' ');
 }
 
-client.login(token); //change it to your token
+client.login(token);
